064a3a22e1ff08f20e888473b85e61e167555078
iwperez85 <iwperez85@yahoo.com>
2015-01-13 11:04:57 -0500

    Refactored css to sass

--- app/scripts/landing.js
+++ app/scripts/landing.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
     $('.hero-content h3').click(function(){
       var subText = $(this).text();
        $(this).text(subText + "!");
+       $("h3").hover(function(){
+    $("h3").css("color","blue");
+    },function(){
+    $("h3").css("color","red");
+
+});
     });
  
    var onHoverAction = function(event) {
@@ -15,4 +21,11 @@ $(document).ready(function() {
    };
  
     $('.selling-points .point').hover(onHoverAction, offHoverAction);
+     $("p").on("click",function(){
+    $(this).animate({fontSize:"+=2px"});
   });
+
+     $( "h1" ).click(function() {
+  $( "h1" ).fadeOut( "slow");
+});
+});
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -299,6 +299,12 @@ $(document).ready(function() {
     $('.hero-content h3').click(function(){
       var subText = $(this).text();
        $(this).text(subText + "!");
+       $("h3").hover(function(){
+    $("h3").css("color","blue");
+    },function(){
+    $("h3").css("color","red");
+
+});
     });
  
    var onHoverAction = function(event) {
@@ -312,7 +318,14 @@ $(document).ready(function() {
    };
  
     $('.selling-points .point').hover(onHoverAction, offHoverAction);
+     $("p").on("click",function(){
+    $(this).animate({fontSize:"+=2px"});
   });
+
+     $( "h1" ).click(function() {
+  $( "h1" ).fadeOut( "slow");
+});
+});
 });
 
 ;
